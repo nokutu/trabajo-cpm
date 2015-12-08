04be91d1a36d3fcb629bc062312dc32d352c63e5
Jorge López Fueyo <nokutu@openmailbox.org>
2015-12-08 12:33:14 +0100

    Added application plugin

--- test/homework/ParserTest.java
+++ test/homework/ParserTest.java
@@ -16,15 +16,14 @@ public class ParserTest {
   public void parseTest() {
     File f = null;
     PrintWriter w = null;
-
     try {
       f = new File("temp.txt");
       w = new PrintWriter(f);
-      w.println("1;2;3;4;5");
-      w.println("1;2;3;4;5");
+      w.println("0;1;2;3;4");
+      w.println("0;1;2;3;4");
       for (String[] line : Parser.parse(f)) {
         for (int i = 0; i < 5; i++) {
-          assertEquals(i + 1, line[i]);
+          assertEquals(i, line[i]);
         }
       }
     } catch (FileNotFoundException e) {
@@ -36,5 +35,4 @@ public class ParserTest {
       f.delete();
     }
   }
-
 }
